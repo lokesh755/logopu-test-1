85d58a3076792510f78be1e12882176dcbf2eeb4
Lokesh Gopu <lokesh755@github.com>
2021-02-09 12:37:45 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,19 +1,3 @@
-$ORG = "testorg755"
-$REPO = "logopu-test"
-
-#$GITHUB_TOKEN = ${{ secrets.CANARY_TOKEN }}
-
-
-$releases = "https://api.github.com/repos/actions/runner/releases"
-
-Write-Host Determining latest release
-
-$assets = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].assets
-
-$latestVersionWindowsRunnerURL = $assets | Where-Object { $_.browser_download_url -like "*win*" } | Select -First 1 -Property browser_download_url
-
-mkdir C:\actions-runner 
-
 cd C:\actions-runner
 
 Invoke-WebRequest -Uri $latestVersionWindowsRunnerURL.browser_download_url -OutFile "runner.zip"
@@ -25,3 +9,32 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem ;
 cd C:\
 
 Remove-Item -Recurse C:\actions-runner -Force -ErrorAction SilentlyContinue
+
+
+
+Write-Host "Get Registration Token"
+
+$registrationTokenUrl = "https://api.github.com/repos/testorg755/logopu-test/actions/runners/registration-token"
+
+$headers = @{
+ "Content-Type"="application/json"
+ "Authorization"= "token 434d08c2a7632651e4b94a34a2a78b3993ce2d11"
+} 
+
+$registrationToken = ((Invoke-WebRequest -Uri $registrationTokenUrl -Method 'Post' -Headers $headers).Content | ConvertFrom-Json).token
+
+Write-Host "Configure runner"
+
+
+ 
+.\config.cmd --url https://github.com/testorg755/logopu-test --token $registrationToken --name windows-runner --labels canary --unattended --replace --runasservice
+
+
+If (Get-ChildItem -Recurse .\_diag | Select-String "Listening for Jobs")
+{
+    Write-Host "Found"
+}
+else {
+    Write-Host "Not Found"
+}
+    
